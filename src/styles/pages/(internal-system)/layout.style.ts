258d98dd5f4aea53b7db2040e125f448ac8f9707
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-08-16 13:13:25 -0300

    feat: update layout.style in internal system

--- src/styles/pages/(internal-system)/layout.style.ts
+++ src/styles/pages/(internal-system)/layout.style.ts
@@ -9,15 +9,6 @@ export const DrawerHeader = styled('div')(({ theme }) => ({
   ...theme.mixins.toolbar
 }))
 
-export const DrawerFoother = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
-  padding: theme.spacing(11.2, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar
-}))
-
 export const boxStyle = { display: 'flex' }
 
 export const boxStyle2 = {
