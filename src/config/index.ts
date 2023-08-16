b310d8d858b9878abf2cd7291fc5ffd05cbd174c
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-08-16 08:29:08 -0300

    fix: delete all file structure

--- src/config/index.ts
+++
@@ -1,7 +0,0 @@
-import * as dotenv from 'dotenv'
-
-dotenv.config()
-
-export default {
-  apiBaseURL: process.env.API_BASE_URL
-}
--- src/services/axios/axiosClient.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-import config from '../../config/index'
-
-const { apiBaseURL } = config
-
-export const axiosClient = axios.create({
-  baseURL: apiBaseURL
-})
