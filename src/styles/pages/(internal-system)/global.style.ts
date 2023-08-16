b75f55b3022a61eb9e30439ed3d66e13efb89fdf
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-08-16 13:11:50 -0300

    feat: create global.style in internal system

---
+++ src/styles/pages/(internal-system)/global.style.ts
@@ -0,0 +1,11 @@
+export const buttonStyle = {
+  width: ' 30vw',
+  height: '30vh',
+  margin: '0 1em 2em 0',
+  backgroundColor: '#fff',
+  flexDirection: 'column'
+}
+
+export const iconStyle = {
+  fontSize: '10em'
+}
