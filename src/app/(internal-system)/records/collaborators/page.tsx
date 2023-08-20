9bdf23fa52681d2a957861800b53f26cfec2ea92
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-08-20 14:02:31 -0300

    feat: update Collaborators

--- src/app/(internal-system)/records/collaborators/page.tsx
+++ src/app/(internal-system)/records/collaborators/page.tsx
@@ -1,19 +1,18 @@
 'use client'
 
 import * as React from 'react'
-import { Button, Typography, type SvgIconTypeMap } from '@mui/material'
+import { Button, Typography, type SvgIconTypeMap, Box } from '@mui/material'
 import { useRouter } from 'next/navigation'
 import { type OverridableComponent } from '@mui/material/OverridableComponent'
 
 import { listIcons } from './constants'
-import { DrawerFoother } from '@/styles/pages/(internal-system)/records/collaborators/collaborators.style'
-import { buttonStyle, iconStyle } from '@/styles/pages/(internal-system)/global.style'
+import { buttonStyle, iconStyle, boxStyle } from '@/styles/pages/(internal-system)/global.style'
 
 export default function Collaborators () {
   const router = useRouter()
 
   return (
-    <>
+    <Box sx={boxStyle}>
     {
       listIcons.map((departmentListAndIcons: Array<string | (OverridableComponent<SvgIconTypeMap<Record<string, unknown>, 'svg'>> & {
         muiName: string
@@ -34,8 +33,7 @@ export default function Collaborators () {
         )
       })
     }
-    <DrawerFoother />
-    </>
+    </Box>
 
   )
 }
