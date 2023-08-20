2891ce0f9ebfcef2a7e7d27a85c4875d3a0f5974
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-08-20 13:55:25 -0300

    feat: create ComponentPath

--- src/app/(internal-system)/layout.tsx
+++ src/app/(internal-system)/layout.tsx
@@ -6,6 +6,7 @@ import CssBaseline from '@mui/material/CssBaseline'
 import HeaderComponent from './components/headerComponent'
 import DrawerComponent from './components/drawerComponent'
 import { DrawerHeader, boxStyle, boxStyle2 } from '@/styles/pages/(internal-system)/layout.style'
+import ComponentPath from './components/componentPath'
 
 export default function Layout (props: {
   children: React.ReactNode
@@ -17,6 +18,7 @@ export default function Layout (props: {
         <DrawerComponent />
         <Box component="main" sx={boxStyle2}>
           <DrawerHeader />
+          <ComponentPath />
           {props.children}
         </Box>
       </Box>
