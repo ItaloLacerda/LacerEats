920d24c949abf7ee167f0071e17a54e2fd421e3b
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-08-20 14:01:33 -0300

    feat: update Records

--- src/app/(internal-system)/records/page.tsx
+++ src/app/(internal-system)/records/page.tsx
@@ -1,19 +1,19 @@
 'use client'
 
 import * as React from 'react'
-import { Button, Typography, type SvgIconTypeMap } from '@mui/material'
+import { Button, Typography, type SvgIconTypeMap, Box } from '@mui/material'
 import { useRouter } from 'next/navigation'
 import { type OverridableComponent } from '@mui/material/OverridableComponent'
 
 import { listIcons } from './constants'
 import { DrawerFoother } from '@/styles/pages/(internal-system)/records/records.style'
-import { buttonStyle, iconStyle } from '@/styles/pages/(internal-system)/global.style'
+import { buttonStyle, iconStyle, boxStyle } from '@/styles/pages/(internal-system)/global.style'
 
 export default function Records () {
   const router = useRouter()
 
   return (
-    <>
+    <Box sx={boxStyle}>
     {
       listIcons.map((departmentListAndIcons: Array<string | (OverridableComponent<SvgIconTypeMap<Record<string, unknown>, 'svg'>> & {
         muiName: string
@@ -35,7 +35,7 @@ export default function Records () {
       })
     }
     <DrawerFoother />
-    </>
+    </Box>
 
   )
 }
