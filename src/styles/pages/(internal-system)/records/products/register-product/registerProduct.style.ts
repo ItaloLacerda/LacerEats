e56cd206ad60f1858ec4f27ce7d9702d4ac02af5
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-08-25 14:47:26 -0300

    feat: create RegisterProduct page

---
+++ src/styles/pages/(internal-system)/records/products/register-product/registerProduct.style.ts
@@ -0,0 +1,12 @@
+export const boxStyle = {
+  width: '65vw',
+  margin: 'auto',
+  height: '75vh',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-evenly'
+}
+
+export const typographyStyle = {
+  color: '#000'
+}
