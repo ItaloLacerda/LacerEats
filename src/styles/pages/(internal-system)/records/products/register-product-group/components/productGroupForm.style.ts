abca6bba81c3004dade4b9da9b20256073839972
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-08-25 09:54:25 -0300

    feat: create ProductGroupForm.style

---
+++ src/styles/pages/(internal-system)/records/products/register-product-group/components/productGroupForm.style.ts
@@ -0,0 +1,16 @@
+export const boxStyle = {
+  display: 'flex',
+  justifyContent: 'space-between'
+}
+
+export const inputStyle = {
+  minWidth: '21vw'
+}
+
+export const roleStyle = {
+  width: '18vw'
+}
+
+export const buttonStyle = {
+  width: '18vw'
+}
